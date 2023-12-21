81ed04dcc5fcc9d7e42cba5a7574a255f27a9dff
nskhu12 <nskhu12@gmail.com>
2023-12-21 15:02:41 +0400

    add custom fields to identity

---
+++ MVCCasino/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVCCasino.Models;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string IdNumber { get; set; }
+}
--- MVCCasino/Program.cs
+++ MVCCasino/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVCCasino.Data;
+using MVCCasino.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+// builder.Services.AddIdentity<User, IdentityRole>()
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
