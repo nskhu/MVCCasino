204385b5e25178a8733aab70ac69daa424ad4346
nskhu12 <nskhu12@gmail.com>
2023-12-24 14:56:36 +0400

    add wallet model

---
+++ MVCCasino/Models/Wallet.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCCasino.Models;
+
+public class Wallet
+{
+    public int WalletId { get; set; }
+    public string UserId { get; set; }
+    public decimal CurrentBalance { get; set; } 
+}
